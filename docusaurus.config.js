c36e55f0dccf8baee7db0ce0a046bf133a3947cc
FranckLM3 <64458289+FranckLM3@users.noreply.github.com>
2025-12-24 10:37:04 +0100

    Change project URL and base URL for deployment
    Updated the URL and base URL for the project configuration.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,8 +12,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Configuration GitHub pages (sera changé en domaine personnalisé une fois le DNS configuré)
-  url: 'https://francklm3.github.io',
-  baseUrl: '/hackathon-nature-en-ville/',
+  // Configuration domaine personnalisé
+  url: 'https://hackathon-nature-ville.ledonut-marseille.com',
+  baseUrl: '/',
+
 
   // Configuration GitHub pages
   organizationName: 'FranckLM3', // Votre nom d'utilisateur GitHub
